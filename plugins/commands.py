8a6d20d3b91505d05aa2eada7b53b99957c296a6
ɢx  ₪ᴇᴏ <89633118+GxNeo@users.noreply.github.com>
2022-01-13 17:00:19 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -207,11 +207,11 @@ async def start(client, message):
         f_caption = f"{files.file_name}"
     buttons = [
                     [
-                        InlineKeyboardButton('ᴄʟᴏsᴇ', callback_data="close_pages"),
-                        InlineKeyboardButton('sʜᴀʀᴇ', url="https://t.me/share/url?url=**😱%20സിനിമ%20ഭ്രാന്തൻ.²·⁰%20😱%0A%0Aഏത്%20അർധരാത്രി%20ചോദിച്ചാലും%20പടം%20കിട്ടും,%20ലോകത്തിലെ%20ഒട്ടുമിക്ക%20ഭാഷകളിലുമുള്ള%20സിനിമകളുടെ%20കളക്ഷൻ..%20❤️%0A%0A👇%20GROUP%20LINK%20👇%0A@CinemaBranthen%0A@CinemaBranthen%0A@CinemaBranthen**")
+                        InlineKeyboardButton('🚸 ᴅᴇʟᴇᴛᴇ', callback_data="close_pages"),
+                        InlineKeyboardButton('💞 sʜᴀʀᴇ', url="https://t.me/share/url?url=**😱%20സിനിമ%20ഭ്രാന്തൻ.²·⁰%20😱%0A%0Aഏത്%20അർധരാത്രി%20ചോദിച്ചാലും%20പടം%20കിട്ടും,%20ലോകത്തിലെ%20ഒട്ടുമിക്ക%20ഭാഷകളിലുമുള്ള%20സിനിമകളുടെ%20കളക്ഷൻ..%20❤️%0A%0A👇%20GROUP%20LINK%20👇%0A@CinemaBranthen%0A@CinemaBranthen%0A@CinemaBranthen**")
                     ],
                     [
-                        InlineKeyboardButton('💬 ᴅᴏᴡɴʟᴏᴀᴅ sᴜʙᴛɪᴛɪʟᴇ 💬', url="https://t.me/subtitle_dl_bot")
+                        InlineKeyboardButton('🌿 Fɪʟᴇ sɪᴢᴇ【{get_size(file.file_size)}】🌿')
                     ]
                     ]
     await client.send_cached_media(
