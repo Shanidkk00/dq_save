328053e68090a807b9df9cab33ecda45874230be
ɢx  ₪ᴇᴏ <89633118+GxNeo@users.noreply.github.com>
2022-01-15 15:21:34 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -6,7 +6,6 @@ import ast
 from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 from Script import script
 import pyrogram
-from OMDB import get_movie_info
 from database.connections_mdb import active_connection, all_connections, delete_connection, if_active, make_active, make_inactive
 from info import ADMINS, AUTH_CHANNEL, AUTH_USERS, CUSTOM_FILE_CAPTION, AUTH_GROUPS, P_TTI_SHOW_OFF, IMDB, SINGLE_BUTTON, SPELL_CHECK_REPLY, IMDB_TEMPLATE
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
@@ -23,6 +22,7 @@ from database.filters_mdb import(
 import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
+
 
 BUTTONS = {}
 SPELL_CHECK = {}
