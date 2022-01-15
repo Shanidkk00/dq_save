1f48e29f2ce2acecc9bd043561f4a7d07555c387
ɢx  ₪ᴇᴏ <89633118+GxNeo@users.noreply.github.com>
2022-01-15 18:46:02 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -205,10 +205,20 @@ async def start(client, message):
             f_caption=f_caption
     if f_caption is None:
         f_caption = f"{files.file_name}"
+    buttons = [
+                    [
+                        InlineKeyboardButton('ᴄʟᴏsᴇ', callback_data="close_pages"),
+                        InlineKeyboardButton('sʜᴀʀᴇ', url="https://t.me/share/url?url=**😱%20സിനിമ%20ഭ്രാന്തൻ.²·⁰%20😱%0A%0Aഏത്%20അർധരാത്രി%20ചോദിച്ചാലും%20പടം%20കിട്ടും,%20ലോകത്തിലെ%20ഒട്ടുമിക്ക%20ഭാഷകളിലുമുള്ള%20സിനിമകളുടെ%20കളക്ഷൻ..%20❤️%0A%0A👇%20GROUP%20LINK%20👇%0A@CinemaBranthen%0A@CinemaBranthen%0A@CinemaBranthen**")
+                    ],
+                    [
+                        InlineKeyboardButton('💬 ᴅᴏᴡɴʟᴏᴀᴅ sᴜʙᴛɪᴛɪʟᴇ 💬', url="https://t.me/subtitle_dl_bot")
+                    ]
+                    ]
     await client.send_cached_media(
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
+        reply_markup=InlineKeyboardMarkup(buttons)
         )
                     
 
