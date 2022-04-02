39f576e6a7f44c3ab3ba2c3cab440a4c6d966183
ɢᴛ ᠰ ʜᴇʀᴏ <92996711+TGGTHERO@users.noreply.github.com>
2022-04-02 18:31:26 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -65,7 +65,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"▫ {get_size(file.file_size)} ▸ {file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"🔖 {get_size(file.file_size)} ▫ {file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
@@ -624,7 +624,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"▫ {get_size(file.file_size)} ▸ {file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"🔖 {get_size(file.file_size)} ▫ {file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
